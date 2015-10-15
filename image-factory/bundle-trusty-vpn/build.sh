b4cab9be9a79954b9ae2419747a56c90c508085c
root <you@example.com>
2015-10-15 17:49:20 +0200

    1ere modif

--- image-factory/bundle-trusty-vpn/build.sh
+++ image-factory/bundle-trusty-vpn/build.sh
@@ -6,4 +6,4 @@ IMG_UBUNTU_TRUSTY="ae3082cb-fac1-46b1-97aa-507aaa8f184f"
 SELF_PATH=`dirname "$0"`
 
 # bundle-build.sh BASENAME CW_BUNDLE_ID BUNDLE_PATH SRC_IMG IMG_OS
-bash "$SELF_PATH/../bundle-build.sh "bundle-trusty-vpn" "VPN" "bundle-trusty-vpn" "$IMG_UBUNTU_TRUSTY" "Ubuntu"
+bash "$SELF_PATH/../bundle-build.sh" "bundle-trusty-vpn" "VPN" "bundle-trusty-vpn" "$IMG_UBUNTU_TRUSTY" "Ubuntu"
